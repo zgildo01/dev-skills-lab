21399a29a89248b1aa62d0ce7a7a7c540fceb492
zgildo01 <zzaragozagildo@gmail.com>
2022-09-20 18:39:35 -0400

    set up data file

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {name: 'Zeus', enrolled: true, _id: 125223},
+  {name: 'Brandon', enrolled: false, _id: 127904},
+  {name: 'Sergio', enrolled: false, _id: 139608},
+]
+
+export {
+  students
+}
