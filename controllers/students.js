bbe40577bfe493e014bd10c6ce447fbfb1861fc0
zgildo01 <zzaragozagildo@gmail.com>
2022-09-21 17:11:51 -0400

    add input functionality

--- controllers/students.js
+++ controllers/students.js
@@ -13,6 +13,34 @@ function index(req, res) {
   })
 }
 
+function newStudent(req, res) {
+  res.render('students/new')
+}
+
+function create(req, res) {
+  Student.create(req.body)
+  .then(student => {
+    console.log(student)
+    res.redirect('/students')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function show(req, res) {
+  Student.findById(req.params.id)
+  .then(student => {
+    res.render('students/show', {
+      student: student
+    })
+  })
+}
+
 export {
-  index
+  index,
+  newStudent as new,
+  create,
+  show,
 }
--- routes/students.js
+++ routes/students.js
@@ -5,6 +5,9 @@ const router = Router()
 
 /* GET students listing. */
 router.get('/', studentsCtrl.index);
+router.get('/new', studentsCtrl.new);
+router.get('/:id', studentsCtrl.show);
+router.post('/', studentsCtrl.create)
 
 export {
   router
