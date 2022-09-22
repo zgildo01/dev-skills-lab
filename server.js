a17a524b8c069c90c294459b75d6445da4e4f61b
zgildo01 <zzaragozagildo@gmail.com>
2022-09-22 12:14:52 -0400

    add method override

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
+import methodOverride from 'method-override'
 import './config/database.js'
 
 // import routers
@@ -29,6 +30,7 @@ app.use(
     path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
   )
 )
+app.use(methodOverride('_method'))
 app.use(function(req, res, next) {
   req.time = new Date().toLocaleTimeString()
   next()
